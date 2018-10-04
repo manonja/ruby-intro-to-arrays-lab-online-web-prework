1a5830af004e7fcb592467031737472286c2a045
manon jacquin <manon.jacquin@gmail.com>
2018-10-04 18:23:11 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,6 +15,14 @@ def third_element(array)
   return array[2]
 end
 
+def last_element(array)
+  return array.last
+end 
+
+def first_element_with_array_methods(array)
+  return array[0]
+end
+
 
 
   
